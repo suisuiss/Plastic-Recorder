70109ea8a07ed76c66437a1c109e7c045ac3e18b
Ms.THANAWAN PANAPONGPAISAN <63130500211@st.sit.kmutt.ac.th>
2022-04-20 17:29:42 +0700

    Don't edit main pls

--- plastic_recorder/lib/main.dart
+++ plastic_recorder/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:plastic_recorder/today_screen.dart';
+import 'package:plastic_recorder/firstpage.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Today());
+    return MaterialApp(home: firstPage());
   }
 }
