21c5ee25c7e98f868f445d3d5f6e05caaa6a54c7
iceing11 <ice_iceing@hotmail.com>
2022-04-24 05:34:17 +0700

    [ADD] pageview - indicator needs fixing

--- plastic_recorder/lib/recommend.dart
+++ plastic_recorder/lib/recommend.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:plastic_recorder/footer.dart';
 import 'package:plastic_recorder/header.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter/animation.dart';
+import 'package:page_view_indicators/circle_page_indicator.dart';
+import 'package:page_view_indicators/animated_circle_page_indicator.dart';
 
 class Recommend extends StatefulWidget {
   const Recommend({Key? key}) : super(key: key);
@@ -14,6 +15,9 @@ class Recommend extends StatefulWidget {
 class _RecommendState extends State<Recommend> {
   @override
   Widget build(BuildContext context) {
+    final ValueNotifier<int> _pageNotifier = new ValueNotifier<int>(0);
+    int test = 0;
+
     // Full screen width and height
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
@@ -28,19 +32,15 @@ class _RecommendState extends State<Recommend> {
     // Height (without status and toolbar)
     double height3 = height - padding.top - kToolbarHeight;
 
-    var now = new DateTime.now();
-    String formattedDate = DateFormat('MMMd').format(now);
+    final controller = PageController(initialPage: 1);
 
-    int avg = 2500;
-    int points = 3000;
-    double progress = points / avg;
+    var bottlesCard = RecommendCard(
+        'Plastic Bottles',
+        'assets/bottles.png',
+        'Small plastic bottles might be convenient, but they are even more wasteful than large plastic bottles. As an alternative, you should try using refillable bottles instead of single use. You can also use blah blah and blah blah for an even greener option...',
+        UniqueKey());
 
-    String display = ' points below average!';
-    if (progress > 1) {
-      display = ' points above average!';
-    }
-
-    var color = ColorTween(begin: Colors.green, end: Colors.red);
+    List<Widget> totalCards = <Widget>[bottlesCard, bottlesCard, bottlesCard];
 
     return Scaffold(
       //You should use `Scaffold` if you have `TextField` in body.
@@ -58,9 +58,32 @@ class _RecommendState extends State<Recommend> {
             //Body Container
             Expanded(
               child: SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: RecommendCard('Test'),
-              ),
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 410,
+                        width: 300,
+                        child: PageView(
+                          controller: controller,
+                          children: totalCards,
+                          onPageChanged: (index) {
+                            setState(() {
+                              _pageNotifier.value = index;
+                              print(_pageNotifier.value);
+                            });
+                          },
+                        ),
+                      ),
+                      Text(_pageNotifier.value.toString()),
+                      Center(
+                        child: CirclePageIndicator(
+                          currentPageNotifier: _pageNotifier,
+                          itemCount: totalCards.length,
+                        ),
+                      ),
+                    ],
+                  )),
             ),
 
             //Footer Container
@@ -78,12 +101,62 @@ class _RecommendState extends State<Recommend> {
 }
 
 class RecommendCard extends StatelessWidget {
-  const RecommendCard(this.title);
+  const RecommendCard(this.title, this.imagePath, this.desc, Key? key)
+      : super(key: key);
 
   final String title;
+  final String imagePath;
+  final String desc;
 
   @override
   Widget build(BuildContext context) {
-    return Text(title);
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      height: 410,
+      width: 400,
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            DefaultTextStyle(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              child: Text(
+                title,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                child: Image.asset(
+                  imagePath,
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+            ),
+            DefaultTextStyle(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              child: Text(
+                desc,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
