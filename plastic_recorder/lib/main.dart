7db3c886a10b1c4347209c734b619bf030ecdaa5
nontakorn223 <69671412+nontakorn223@users.noreply.github.com>
2022-04-24 15:42:30 +0700

    login_register_calender

--- plastic_recorder/lib/main.dart
+++ plastic_recorder/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:plastic_recorder/calender.dart';
 import 'package:plastic_recorder/firstpage.dart';
+import 'package:plastic_recorder/register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: firstPage());
+    return MaterialApp(home: calender());
   }
 }
 
