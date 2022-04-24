f0ff2f653ecb46c6d3c72f577e21eb03a1e34b3a
maxky49221 <ratchanon.maxky@mail.kmutt.ac.th>
2022-04-24 16:12:02 +0700

    [UPDATE TODAY PAGE]

--- plastic_recorder/lib/today_screen.dart
+++ plastic_recorder/lib/today_screen.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 
 class Today extends StatelessWidget {
   Today({Key? key}) : super(key: key);
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +21,8 @@ class Today extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.all(20),
                 color: Color(0xffF6F6F8),
-                child: Column(
+                child: SingleChildScrollView(
+                  child: Column(
                   children: [
                     Container(
                       alignment: Alignment.topCenter,
@@ -32,16 +34,18 @@ class Today extends StatelessWidget {
                             decoration: TextDecoration.none)  ),
                     ),
                     
-                    Box(pieces: 2, points: 2),
-                    Box(pieces: 3, points: 3),
-                    Box(pieces: 5, points: 10),
-                    Box(pieces: 1, points: 1),
-                    Box(pieces: 10, points: 20),
+                    Box(pieces: 2, points: 2 , name: 'PET' , pic: 'assets/im1.png' ,),
+                    Box(pieces: 3, points: 3 , name:'PE-HD' , pic:'assets/im2.png' ,),
+                    Box(pieces: 5, points: 10, name:'PVC' , pic:'assets/im3.png' ,),
+                    Box(pieces: 1, points: 1, name: 'PE-LD', pic:'assets/im4.png' ,),
+                    Box(pieces: 10, points: 20, name: 'PP',pic:'assets/im5.png' ,),
+                    Box(pieces: 10, points: 20, name: 'PS',pic:'assets/im6.png' ,),
+                    Box(pieces: 10, points: 20, name: 'O',pic:'assets/im7.png' ,),
                     Container(
                       padding: EdgeInsets.all(8),
                       alignment: Alignment.bottomRight,
                       child: Text(
-                        'Total: 36 point(s)',
+                        'Total: $sum  point(s)',
                         style: TextStyle(
                             fontSize: 15,
                             color: Colors.black,
@@ -52,6 +56,7 @@ class Today extends StatelessWidget {
                     Button(),
                   ],
                 ),
+                )   
               ),
             ),
             footer(),
@@ -61,32 +66,45 @@ class Today extends StatelessWidget {
     );
   }
 }
+var sum = 0;
 
 class Box extends StatefulWidget {
   final int pieces;
   final int points;
+  final String pic;
+  final String name;
 
-  const Box({Key? key, required this.pieces, required this.points})
+  const Box({Key? key, required this.pieces, required this.points, required this.pic, required this.name})
       : super(key: key);
 
   @override
   State<Box> createState() =>
-      _BoxState(pieces: this.pieces, points: this.points);
+      _BoxState(pieces: this.pieces, points: this.points, pic: this.pic, name: this.name);
 }
 
 class _BoxState extends State<Box> {
   int pieces;
   int points;
+  String pic;
+  String name;
 
-  _BoxState({required this.pieces, required this.points});
+
+  _BoxState({required this.pieces, required this.points, required this.pic, required this.name,});
+
+  int sumpoint(){
+    sum += points;
+    return sum;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
     return Container(
       // padding: EdgeInsets.all(12.0),
-      margin: const EdgeInsets.all(3.0),
-      width: 450.0,
-      height: 70.0,
+      margin: const EdgeInsets.all(5.0),
+      width: deviceWidth*0.9,
+      height: deviceHeight*0.1,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
@@ -102,25 +120,32 @@ class _BoxState extends State<Box> {
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Image.asset(
-                'assets/water.png',
-                scale: 0.2,
+                pic,
+                scale: 0.1,
+                width: 100,
               )),
-          Text(
-            'Bottle',
+          Container(
+            width: 125,
+            child:
+            Text(
+            name,
             style: TextStyle(
                 fontSize: 20,
                 color: Colors.black,
                 decoration: TextDecoration.none),
+          ), 
           ),
+          
           Column(
             children: [
               Container(
                   padding: const EdgeInsets.all(5.0),
-                  margin: const EdgeInsets.only(top: 5.5),
+                  margin: const EdgeInsets.only(top: 3.5),
                   decoration: BoxDecoration(
                     border: Border.all(width: 1.0, color: Color(0xffFF9F1D)),
                     borderRadius: BorderRadius.circular(10),
@@ -147,7 +172,7 @@ class _BoxState extends State<Box> {
                   width: 125.0,
                   height: 28.0,
                   child: Text(
-                    '$points point(s)',
+                    '$points point(s) $sumpoint()',
                     style: TextStyle(
                         fontSize: 15,
                         color: Color.fromARGB(255, 255, 255, 255),
@@ -180,17 +205,8 @@ class _ButtonState extends State<Button> {
         style: ElevatedButton.styleFrom(
           primary: Color(0xffFF9F1D),
         ),
-        // ButtonStyle(
-        //   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-        //       RoundedRectangleBorder(
-        //           borderRadius: BorderRadius.circular(13.0),
-        //           side: BorderSide(
-        //             color: Color(0xffFF9F1D),
-        //           )
-        //       )
-        //   ),
-        // ),
       ),
     );
   }
 }
+
