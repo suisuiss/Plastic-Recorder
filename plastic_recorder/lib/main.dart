e8b1006f6081df2ad41ea4df2c7fbffa1385cb2f
nontakorn223 <69671412+nontakorn223@users.noreply.github.com>
2022-04-26 15:58:41 +0700

    setup route

--- plastic_recorder/lib/main.dart
+++ plastic_recorder/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:plastic_recorder/calender.dart';
+import 'package:plastic_recorder/compare.dart';
 import 'package:plastic_recorder/firstpage.dart';
+import 'package:plastic_recorder/login_screen.dart';
+import 'package:plastic_recorder/recommend.dart';
+import 'package:plastic_recorder/register.dart';
 
 
 void main() {
@@ -12,7 +17,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: firstPage());
+    return MaterialApp(
+       
+      routes: {
+        '/':(context) => firstPage(),
+        '/login': (context) => LoginScreen(),
+        '/register' :(context) => register(),
+        '/calender' :(context) => calender(),
+        '/compare' :(context) => Compare(),
+        'recommendation' :(context) => Recommend(),
+        //'/profile' :(context) => (),
+        //'/add' :(context) => Add(), 
+        //'/today' : (context) => today(),
+        
+      
+      },
+       
+
+      );
   }
 }
 
