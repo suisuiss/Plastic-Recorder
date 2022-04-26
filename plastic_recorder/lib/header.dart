fa95e36248545d409bf49d28991d6958ef209e6f
Pipeparin12 <79441231+Pipeparin12@users.noreply.github.com>
2022-04-26 14:35:41 +0700

    .

---
+++ plastic_recorder/lib/header.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class header extends StatefulWidget {
+  const header({Key? key}) : super(key: key);
+
+  @override
+  State<header> createState() => _headerState();
+}
+
+class _headerState extends State<header> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Icon(
+              Icons.chevron_left_outlined,
+              size: 45,
+              color: Color(0xffFF9F1D),
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 60,
+            )
+          ]),
+        ]),
+      ),
+    );
+  }
+}
