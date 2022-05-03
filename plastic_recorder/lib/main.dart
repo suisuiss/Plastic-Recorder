3dc540adb2a484875b449b238dc4732da7e8fc4a
Thanawan Panapongpaisan <74504881+suisuiss@users.noreply.github.com>
2022-05-03 20:56:10 +0700

    Update main.dart

--- plastic_recorder/lib/main.dart
+++ plastic_recorder/lib/main.dart
@@ -1,46 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:plastic_recorder/add_screen.dart';
+import 'package:plastic_recorder/calender.dart';
+import 'package:plastic_recorder/compare.dart';
 import 'package:plastic_recorder/firstpage.dart';
-<<<<<<< Updated upstream
-
-=======
 import 'package:plastic_recorder/login_screen.dart';
+import 'package:plastic_recorder/profile_page.dart';
 import 'package:plastic_recorder/recommend.dart';
 import 'package:plastic_recorder/register.dart';
-import 'package:plastic_recorder/add_screen.dart';
 import 'package:plastic_recorder/today_screen.dart';
->>>>>>> Stashed changes
+import 'package:plastic_recorder/add_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
+ 
+
+ 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-<<<<<<< Updated upstream
-    return MaterialApp(home: firstPage());
-=======
-    return MaterialApp(
-       
+
+     
+    return MaterialApp( 
       routes: {
         '/':(context) => firstPage(),
         '/login': (context) => LoginScreen(),
         '/register' :(context) => register(),
-        '/calender' :(context) => calender(),
+        '/calender' :(context) => calender(),        
         '/compare' :(context) => Compare(),
-        'recommendation' :(context) => Recommend(),
-        //'/profile' :(context) => (),
+        '/recommendation' :(context) => Recommend(),
+        '/profile' :(context) => ProfilePage(),
         '/add' :(context) => Add(), 
         '/today' : (context) => Today(),
+
         
       
       },
        
 
       );
->>>>>>> Stashed changes
   }
 }
